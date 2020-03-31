693cd6b2bfe86a6398a5540fb4603c63dab4d7ca
ammarshahin <ammar0ammar612@gmail.com>
2020-03-31 20:13:01 +0200

    Quick Save

--- src/FirstOOPprogram.cpp
+++ src/FirstOOPprogram.cpp
@@ -19,7 +19,7 @@ int main()
     Rect r1;
     r1.l = 10;
     r1.w = 20;
-    uint64_t area = r1.rectArea;
+    uint64_t area = r1.rectArea();
     cout << "area = " << area << endl;
     return 0;
 }
