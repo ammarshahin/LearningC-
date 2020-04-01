12abb8da566107d9cf50f994cfdaef78eee75429
ammarshahin <ammar0ammar612@gmail.com>
2020-04-01 16:47:13 +0200

    Quick Save

--- src/SecondLessonOOP.cpp
+++ src/SecondLessonOOP.cpp
@@ -1,7 +1,75 @@
 /**** 1. Operator OverLoading **********/
 
 #include <iostream>
-#include "../inc./classes.hpp"
+//#include "../inc./classes.hpp"
+
+
+#include <iostream>
+
+class Rect
+{
+    /* The Object(Class) Data */
+    uint32_t l, w;
+    static uint8_t rectCount;
+
+public:
+    /* The Object(Class) Constructors */
+    Rect() // Empty Constructor
+    {
+        this->l = 0;
+        this->w = 0;
+        this->rectCount++;
+    }
+
+    Rect(uint32_t l, uint32_t w)
+    {
+        this->l = l;
+        this->w = w;
+        this->rectCount++;
+    }
+
+    /* The Object(Class) Methods */
+    uint64_t area()
+    {
+        return (uint64_t)l * w;
+    }
+
+    void changeLegnth(uint32_t l)
+    {
+        this->l = l;
+    }
+
+    void changeWidth(uint32_t w)
+    {
+        this->w = w;
+    }
+
+    uint32_t legnth()
+    {
+        return this->l;
+    }
+
+    uint32_t width()
+    {
+        return this->w;
+    }
+
+    Rect operator+(Rect x)
+    {
+        Rect temp;
+        temp.l = this->l + x.l;
+        temp.w = this->w + x.w;
+        return temp;
+    }
+
+    uint8_t rectCounter()
+    {
+        return this->rectCount;
+    }
+
+    /* The Object(Class) Destructor */
+    ~Rect() {}
+};
 
 using namespace std;
 
