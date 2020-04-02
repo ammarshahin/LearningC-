e02d903bfa96ee4110a9871a324219edcd5a7fb4
ammarshahin <ammar0ammar612@gmail.com>
2020-04-02 09:08:57 +0200

    Adding 4rth lesson'

---
+++ src/4rthLessonOOP.cpp
@@ -0,0 +1,14 @@
+/**** 1. Inheritance **********/
+
+#include <iostream>
+#include "../inc./rectClass.hpp"
+
+using namespace std;
+
+int main()
+{
+    rectClass r1(10, 20), r2;
+    cout << "Rectangle area = " << r2.area() << endl;
+
+    return 0;
+}
