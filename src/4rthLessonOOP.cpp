4b62c020c16ea713db1ae58e6b54d55132418ae7
ammarshahin <ammar0ammar612@gmail.com>
2020-04-02 09:15:35 +0200

    Quick Save

--- src/4rthLessonOOP.cpp
+++ src/4rthLessonOOP.cpp
@@ -1,14 +1,113 @@
 /**** 1. Inheritance **********/
 
 #include <iostream>
-#include "../inc./rectClass.hpp"
-
 using namespace std;
+
+class RectClass
+{
+    /* The Object(Class) Data */
+    uint32_t l, w;
+
+public:
+    /* The Object(Class) Constructors */
+    RectClass() // Empty Constructor
+    {
+        this->l = 0;
+        this->w = 0;
+    }
+
+    RectClass(uint32_t l, uint32_t w)
+    {
+        this->l = l;
+        this->w = w;
+    }
+
+    /* The Object(Class) Methods */
+    uint64_t area()
+    {
+        return (uint64_t)l * w;
+    }
+
+    void changeLegnth(uint32_t l)
+    {
+        this->l = l;
+    }
+
+    void changeWidth(uint32_t w)
+    {
+        this->w = w;
+    }
+
+    uint32_t legnth()
+    {
+        return this->l;
+    }
+
+    uint32_t width()
+    {
+        return this->w;
+    }
+
+    /* The Object(Class) Destructor */
+    ~RectClass() {}
+};
+
+class TriClass
+{
+    /* The Object(Class) Data */
+    uint32_t l, w;
+
+public:
+    /* The Object(Class) Constructors */
+    TriClass() // Empty Constructor
+    {
+        this->l = 0;
+        this->w = 0;
+    }
+
+    TriClass(uint32_t l, uint32_t w)
+    {
+        this->l = l;
+        this->w = w;
+    }
+
+    /* The Object(Class) Methods */
+    uint64_t area()
+    {
+        return (uint64_t)0.5 * l * w;
+    }
+
+    void changeLegnth(uint32_t l)
+    {
+        this->l = l;
+    }
+
+    void changeWidth(uint32_t w)
+    {
+        this->w = w;
+    }
+
+    uint32_t legnth()
+    {
+        return this->l;
+    }
+
+    uint32_t width()
+    {
+        return this->w;
+    }
+
+    /* The Object(Class) Destructor */
+    ~TriClass() {}
+};
 
 int main()
 {
-    rectClass r1(10, 20), r2;
-    cout << "Rectangle area = " << r2.area() << endl;
+    RectClass r1(10, 20);
+    cout << "Rectangle area = " << r1.area() << endl;
+
+    TriClass t1(10, 20);
+    cout << "Triangle area = " << t1.area() << endl;
 
     return 0;
 }
