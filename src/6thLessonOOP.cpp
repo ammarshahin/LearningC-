ac5d70122ed515c44328c6939b2f1d5e07d257bc
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 10:57:58 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -1,30 +1,29 @@
 /********** template ************/
-
 #include <iostream>
 using namespace std;
 
 template <class R, class T> // Template parameter
 
-R divfunc(T a, T b)
+class Test
 {
-    return (R)a / b;
-}
+    T a, b;
 
-/*
-R getMax(T a, T b) // Template function
-{
-    return ((a > b) ? a : b);
-}
-*/
+public:
+    Test(T a, T b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    R divfunc() // Template function
+    {
+        return (R)this->a / this->b;
+    }
+};
 
 int main()
 {
-    /*
-    cout << "The g = " << getMax<int, int>(2, 10) << endl;            // replace the T template with int
-    cout << "The g = " << getMax<float, float>(2.1, 10.4343) << endl; // replace the T template with float
-    cout << "The g = " << getMax<double, double>(2.0012, 10) << endl; // replace the T template with double
-    */
-    cout << "The g = " << divfunc<double, int>(10, 2.12) << " " << (double)10 / 2.12 << endl; // replace the T template with double
-
+    Test<double, int> clT(10, 2.1);
+    cout << "The g = " << clT.divfunc() << endl; // replace the R template with double and replace the T template with int
     return 0;
 }
