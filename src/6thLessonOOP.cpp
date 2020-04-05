b8541aff3b50d73128c2bde35af48d79679a46f8
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 12:36:52 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -20,10 +20,11 @@ public:
         return (R)this->a / this->b;
     }
 };
+template <class R, class T> // Template parameter
 
 int main()
 {
-    Test<double, int> clT(10, 2.1);
-    cout << "The g = " << clT.divfunc() << endl; // replace the R template with double and replace the T template with int
+    Test<double, int> clT(10, 2.1); // replace the R template with double and replace the T template with int
+    cout << "The g = " << clT.divfunc() << endl;
     return 0;
 }
