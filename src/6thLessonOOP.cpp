c299fbe1a58684fc77a31c49384bd07b836ede99
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 10:46:27 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -3,17 +3,28 @@
 #include <iostream>
 using namespace std;
 
-template <class T> // Template parameter
+template <class R, class T> // Template parameter
 
-inline T getMax(T a, T b) // Template function
+R divfunc(T a, T b)
+{
+    return a / b;
+}
+
+/*
+R getMax(T a, T b) // Template function
 {
     return ((a > b) ? a : b);
 }
+*/
 
 int main()
 {
-    cout << "The g = " << getMax(2, 10) << endl;                // replace the T template with int
-    cout << "The g = " << getMax(2.1, 10.4343) << endl;         // replace the T template with float
-    cout << "The g = " << getMax<double>(2.1, 10.4343) << endl; // replace the T template with double
+    /*
+    cout << "The g = " << getMax<int, int>(2, 10) << endl;            // replace the T template with int
+    cout << "The g = " << getMax<float, float>(2.1, 10.4343) << endl; // replace the T template with float
+    cout << "The g = " << getMax<double, double>(2.0012, 10) << endl; // replace the T template with double
+    */
+    cout << "The g = " << divfunc<double, int>(10, 2.0012) << endl; // replace the T template with double
+
     return 0;
 }
