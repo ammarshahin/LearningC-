0968a1d3e7eff06474f2c672a4d9c0ee00c19be9
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 10:21:16 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+inline uint8_t getMax(uint8_t a, uint8_t b)
+{
+    return (a > b ? a : b);
+}
+
 int main()
 {
+    cout << getMax(2, 10) << endl;
     return 0;
 }
