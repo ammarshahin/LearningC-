b691b7abc73548a6872d3ffa434c4063b019d96d
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 12:46:55 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-template <class R, class T> // Template parameter
+template <class R, class T, int n = 5> // R & T are Template parameter, n is a global variable 
 
 class Test
 {
@@ -17,14 +17,14 @@ public:
 
     R divfunc() // Template function
     {
+        cout << "n = " << n << endl;
         return (R)this->a / this->b;
     }
 };
-template <class R, class T> // Template parameter
 
 int main()
 {
-    Test<double, int> clT(10, 2.1); // replace the R template with double and replace the T template with int
+    Test<double, int,6> clT(10, 2.1); // replace the R template with double and replace the T template with int
     cout << "The g = " << clT.divfunc() << endl;
     return 0;
 }
