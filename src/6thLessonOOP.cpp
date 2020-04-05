5b8a021ef4201197870b797af25a562748e6ff3e
ammarshahin <ammar0ammar612@gmail.com>
2020-04-05 10:34:18 +0200

    Quick Save

--- src/6thLessonOOP.cpp
+++ src/6thLessonOOP.cpp
@@ -12,9 +12,8 @@ inline T getMax(T a, T b)
 
 int main()
 {
-    cout << "The g = " << getMax<uint8_t>(2, 10) << endl;
-    cout << "The g = " << getMax<int>(2, 10) << endl;
-    cout << "The g = " << getMax<float>(2.1, 10.4343) << endl;
-    cout << "The g = " << getMax<double>(2.0012, 10) << endl;
+    cout << "The g = " << getMax<int>(2, 10) << endl;          // replace the T template with int
+    cout << "The g = " << getMax<float>(2.1, 10.4343) << endl; // replace the T template with float
+    cout << "The g = " << getMax<double>(2.0012, 10) << endl;  // replace the T template with double
     return 0;
 }
