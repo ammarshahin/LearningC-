e9dd6395fe0c0e7b34bc11acd774285172ca92c5
ammarshahin <ammar0ammar612@gmail.com>
2020-04-06 16:00:57 +0200

    Quick Save

---
+++ hakerrankExersise/InputAndOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
