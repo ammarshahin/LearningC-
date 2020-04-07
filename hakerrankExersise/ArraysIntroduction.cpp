2daaace2daf91a5a61e97e4417e05c7d6564ba33
ammarshahin <ammar0ammar612@gmail.com>
2020-04-07 02:27:09 +0200

    Quick Save

--- hakerrankExersise/ArraysIntroduction.cpp
+++ hakerrankExersise/ArraysIntroduction.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int main()
 {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int arr[1000];
     int n;
     cin >> n;
-    
+    int *pArr = new int[n]; // Using Dynamic memory Allocation
+
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d", &pArr[i]);
     }
 
     for (int i = n - 1; i >= 0; i--)
     {
-        printf("%d ", arr[i]);
+        printf("%d ", pArr[i]);
     }
 
     return 0;
