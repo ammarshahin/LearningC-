42be91a09ade6d63aff85e660e3091f608e9d4af
ammarshahin <ammar0ammar612@gmail.com>
2020-04-08 02:35:29 +0200

    Quick Save

--- hakerrankExersise/Strings.cpp
+++ hakerrankExersise/Strings.cpp
@@ -8,9 +8,9 @@ int main()
     string a, b, c;
     cin >> a >> b;
     c = a + b;
-    cout << a.size() << b.size() << endl;
+    cout << a.size() << " " << b.size() << endl;
     cout << c << endl;
-    //cout << a.reserve() << b.reserve() << endl;
+    cout << a.reserve() << b.reserve() << endl;
 
     return 0;
 }
