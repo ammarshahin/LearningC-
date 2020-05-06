3f732c4323e7f6d8633c670be96d8c4b0900ca28
ammarshahin <ammar0ammar612@gmail.com>
2020-05-07 00:12:34 +0200

    Quick Save

--- src/VectorSTL.cpp
+++ src/VectorSTL.cpp
@@ -28,22 +28,28 @@ int vecSum(std::vector<int> v, int size);
  */
 int main()
 {
-    // Declaring a vector
+    // Declaring a vector with Constructor
     std::vector<int> v1(10); // Make a vector with 10 int elements that have an initialization value of 0 (heap)
     std::cout << v1[0] << " " << v1[4] << std::endl;
 
     std::vector<int> v2(3, 7); // Make a vector with 3 char elements that have an initialization value of 7D
     std::cout << v2[0] << " " << v2[1] << std::endl;
 
-    std::cout << v1.size() << std::endl; // return the size of the vector
+    std::vector<int> v3{4, 5, 6, 7}; // Make a vector with 3 char elements that have an initialization value of 7D
+    std::cout << v3[0] << " " << v3[1] << std::endl;
+
+    std::cout << v3.size() << std::endl; // return the size of the vector
 
     std::cout << v2.at(0) << std::endl; // Better way to access the elements of the vector v2.at(0) == v2[0]
 
-    std::vector<int> x; // Make a vector with 0 elements
+    /* 
+  * ex: 
+  */
+    std::vector<int> x; // Make a vector with 0 elements (without Constructor)
     int sum = 0;
 
     for (int i = 0; i < size; ++i)
-        x.push_back(i);
+        x.push_back(i); // add an element to the vec with the value of i (dynamic memory allocation)
 
     sum = vecSum(x, size);
     std::cout << "the Sum of the " << size << " element is : " << sum << std::endl;
