b6de9665d4b35a504f8d0ea4d977c64af5a089e7
ammarshahin <ammar0ammar612@gmail.com>
2020-05-06 21:08:53 +0200

    Quick Save

--- CppForCprogramers/Week1Submission.cpp
+++ CppForCprogramers/Week1Submission.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file Week1Submission.cpp
+ * @author Ammar Shahin (ammar0shahin@gmail.com)
+ * @brief This file is the first week submission in the Cpp for C programers course 
+ * @version 0.1
+ * @date 2020-05-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 // Convert this program to C++
 
 // change to C++ io
@@ -11,28 +22,26 @@
 // inline any short function
 
 #include <iostream>
-#include <vector> 
+#include <vector>
 
 const int N = 40;
 
-void sum(int& p, int n, std::vector<int> d)
+void sum(int &p, int n, std::vector<int> d)
 {
-for(int i = 0; i < n; ++i)
-    p += d[i];
+    for (int i = 0; i < n; ++i)
+        p += d[i];
 }
 
 int main()
 {
-int accum = 0;
+    int accum = 0;
+    std::vector<int> data;
 
-std::vector<int> data;
+    for (int i = 0; i < N; ++i)
+        data.push_back(i);
 
-for(int i = 0; i < N; ++i)
-    data.push_back(i);
+    sum(accum, N, data);
 
-sum(accum, N, data);
-
-std::cout << "sum is " << accum << std::endl; 
-return 0;
-
+    std::cout << "sum is " << accum << std::endl;
+    return 0;
 }
