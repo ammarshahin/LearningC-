ba400d8b3a9b8598a202f87325c498f49ffb92d5
ammarshahin <ammar0ammar612@gmail.com>
2020-05-06 23:19:07 +0200

    Quick Save

--- src/VectorSTL.cpp
+++ src/VectorSTL.cpp
@@ -1,22 +1,51 @@
+/**
+ * @file VectorSTL.cpp
+ * @author Ammar Shahin (ammar0shahin@gmail.com)
+ * @brief This file is to explore the vector standard library
+ * @version v1
+ * @date 2020-05-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+/************************************************************/
+/********************** INCLUDES ****************************/
+/************************************************************/
 #include <iostream>
 #include <vector>
 
+/* Const variabel size */
 const int size = 40;
 
-int arrSum(std::vector<int> v, int size);
+/* Prototypes */
+int vecSum(std::vector<int> v, int size);
 
+/**
+ * @brief The entry function to the program
+ * 
+ * @return int status of the program return to the operating system [0 >> no error, -1 >> error]
+ */
 int main()
 {
     std::vector<int> x;
     int sum = 0;
+
     for (int i = 0; i < size; ++i)
         x.push_back(i);
 
-    sum = arrSum(x, size);
+    sum = vecSum(x, size);
     std::cout << "the Sum of the " << size << " element is : " << sum << std::endl;
 }
 
-int arrSum(std::vector<int> v, int size)
+/**
+ * @brief this function is used to sum the elements of the vector 
+ * 
+ * @param v vector of elements
+ * @param size size of the vector
+ * @return int the sum 
+ */
+int vecSum(std::vector<int> v, int size)
 {
     int sum = 0;
     for (int i = 0; i < size; ++i)
