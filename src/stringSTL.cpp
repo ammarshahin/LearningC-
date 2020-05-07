1879098aa78ed1da0f5636d488c49b8f2668fb04
ammarshahin <ammar0ammar612@gmail.com>
2020-05-07 12:14:42 +0200

    Quick Save

--- src/stringSTL.cpp
+++ src/stringSTL.cpp
@@ -33,18 +33,17 @@ int main()
     std::string str2;               // Empty string (garpage)
     std::cout << str2 << std::endl; // Will not be printed
     str2 = "Ammar Shahin";
-    
+
     std::cout << str1 << str2 << std::endl;
 
-    str1.c_str(str2);
-    
+    str1.append(str2);
+
     std::cout << str1 << str2 << std::endl;
 
-    int size = str1.size(); // Return the size of the string (the count of the letters without the null terminator)
+    int size = str1.size();  // Return the size of the string (the count of the letters without the null terminator)
     int len = str1.length(); // Return the size of the string (the count of the letters without the null terminator)
 
     std::cout << size << " " << len << std::endl;
-    
 
     return 0;
 }
