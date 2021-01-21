e241d83a33a8818e0acacbc1ea10215a319532a7
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-01-21 14:46:27 +0200

    adding the boxIt challenge

---
+++ src/hakerrankExersise/BoxIt.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//Implement the class Box
+//l,b,h are integers representing the dimensions of the box
+
+// The class should have the following functions :
+
+// Constructors:
+// Box();
+// Box(int,int,int);
+// Box(Box);
+
+// int getLength(); // Return box's length
+// int getBreadth (); // Return box's breadth
+// int getHeight ();  //Return box's height
+// long long CalculateVolume(); // Return the volume of the box
+
+//Overload operator < as specified
+//bool operator<(Box& b)
+
+//Overload operator << as specified
+//ostream& operator<<(ostream& out, Box& B)
+
+//Implement the class Box
+//l,b,h are integers representing the dimensions of the box
+
+// The class should have the following functions :
+
+// Constructors:
+// Box();
+// Box(int,int,int);
+// Box(Box);
+
+// int getLength(); // Return box's length
+// int getBreadth (); // Return box's breadth
+// int getHeight ();  //Return box's height
+// long long CalculateVolume(); // Return the volume of the box
+
+//Overload operator < as specified
+//bool operator<(Box& b)
+
+//Overload operator << as specified
+//ostream& operator<<(ostream& out, Box& B)
+
+class Box
+{
+  private:
+    int l;
+    int b;
+    int h;
+
+  public:
+    Box()
+    {
+        this->l = 0;
+        this->b = 0;
+        this->h = 0;
+    }
+
+    Box(int l, int b, int h)
+    {
+        this->l = l;
+        this->b = b;
+        this->h = h;
+    }
+
+    Box(const Box &box)
+    {
+        this->l = box.l;
+        this->b = box.b;
+        this->h = box.h;
+    }
+
+    int getLength()   // Return box's length
+    {
+        return this->l;
+    }
+
+    int getBreadth()   // Return box's breadth
+    {
+        return this->b;
+    }
+
+    int getHeight()   //Return box's height
+    {
+        return this->h;
+    }
+
+    long long CalculateVolume()   // Return the volume of the box
+    {
+        return (long long)this->l * this->b * this->h;
+    }
+
+    bool operator<(Box &b)
+    {
+        bool ret = false;
+        if (this->l < b.l)
+        {
+            ret = true;
+        }
+        else if ((this->l == b.l) && (this->b < b.b))
+        {
+            ret = true;
+        }
+        else if ((this->l == b.l) && (this->b == b.b) && (this->h < b.h))
+        {
+            ret = true;
+        }
+
+        return ret;
+    }
+};
+
+ostream &operator<<(ostream &out, Box &B)
+{
+    out << B.getLength() << " " << B.getBreadth() << " " << B.getHeight();
+    return out;
+};
+
+void check2()
+{
+    int n;
+    cin >> n;
+    Box temp;
+    for (int i = 0; i < n; i++)
+    {
+        int type;
+        cin >> type;
+        if (type == 1)
+        {
+            cout << temp << endl;
+        }
+        if (type == 2)
+        {
+            int l, b, h;
+            cin >> l >> b >> h;
+            Box NewBox(l, b, h);
+            temp = NewBox;
+            cout << temp << endl;
+        }
+        if (type == 3)
+        {
+            int l, b, h;
+            cin >> l >> b >> h;
+            Box NewBox(l, b, h);
+            if (NewBox < temp)
+            {
+                cout << "Lesser\n";
+            }
+            else
+            {
+                cout << "Greater\n";
+            }
+        }
+        if (type == 4)
+        {
+            cout << temp.CalculateVolume() << endl;
+        }
+        if (type == 5)
+        {
+            Box NewBox(temp);
+            cout << NewBox << endl;
+        }
+    }
+}
+
+int main()
+{
+    check2();
+}
