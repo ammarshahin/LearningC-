59c296b8f1fdc77453f1b659beff3badf5e4c4ea
ammarshahin <ammar0ammar612@gmail.com>
2021-02-04 15:01:52 +0200

    start studying multithreading on c++

--- src/Multithreding/1_intro.cpp
+++ src/Multithreding/1_intro.cpp
@@ -3,30 +3,41 @@
 
 static int temp = true;
 
+class myClass
+{
+  public:
+    void operator()(int id)
+    {
+        while (temp > 0)
+        {
+            std::cout << id << std::endl;
+        }
+    }
+};
+
 void foo(int id)
 {
     while (temp > 0)
     {
-        std::cin >> temp;
-        std::cout << "this thread has the id" << id << std::endl;
+        std::cout << id << std::endl;
     }
 }
 
 int main()
 {
     std::thread th1(foo, 1);
-    //std::thread th2(foo, 2);
+    std::thread th2(myClass(), 2);
     //std::thread th3(foo, 3);
 
     while (temp > 0)
     {
-        ;
+        std::cin >> temp;
     }
 
     th1.join();
+    th2.join();
 
     std::cout << "threads are terminated" << std::endl;
-    // th2.join();
-    // th3.join();5
+
     return 0;
 }
