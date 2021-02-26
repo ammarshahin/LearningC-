38aebfcb048648c974cf2d08b7c96b97d80e0ab4
ammarshahin <ammar0ammar612@gmail.com>
2021-02-26 12:21:04 +0200

    aaa

--- src/CppPrimerPlus/chapter4.cpp
+++ src/CppPrimerPlus/chapter4.cpp
@@ -1,3 +1,39 @@
+//! Strings in C++11
+// * C++, recall, has the wchar_t type in addition to char.And C++11 adds the char16_t and char32_t types. It’s possible to create arrays of these types and string literals of these types. C++ uses the L, u, and U prefixes, respectively, for string literals of these types.
+
+//** Here’s an example of how they can be used:
+//*   - wchar_t title[] = L"Chief Astrogator"; // w_char string
+//*   - char16_t name[] = u"Felonia Ripova"; // char_16 string
+//*   - char32_t car[] = U"Humber Super Snipe"; // char_32 string
+
+//* C++11 also supports an encoding scheme for Unicode characters called UTF-8. In this scheme a given character may be stored in anywhere from one 8-bit unit, or octet, to four 8-bit units, depending on the numeric value. C++ uses the u8 prefix to indicate string literals of that type.
+
+//* Another C++11 addition is the raw string. In a raw string, characters simply stand for themselves. For example, the sequence \n is not interpreted as representing the newline character; instead, it is two ordinary characters, a backslash and an n, and it would display as those two characters onscreen.As another example, you can use a simple " inside a string instead of the more awkward \" we used in Listing 4.8. Of course, if you allow a " inside a string literal, you no longer can use it to delimit the ends of a string.Therefore, raw strings use "( and )" as delimiters, and they use an R prefix to identify them as raw strings:
+//*     - cout << R"(Jim "King" Tutt uses "\n" instead of endl.)" << '\n';
+//* This would display the following:
+//*     - Jim "King" Tutt uses \n instead of endl.
+//* The standard string literal equivalent woauld be this :
+//*     - cout << "Jim \"King\" Tutt uses \" \\n\" instead of endl." << '\n';
+//* Here we had to use \\ to display \ because a single \ is interpreted as the first character of an escape sequence.
+
+//* If you press the Enter or Return key while typing a raw string, that not only moves the cursor to the next line onscreen, it also places a carriage return character in the raw string.
+
+//* What if you want to display the combination )" in a raw string? (Who wouldn’t?) Won’t the compiler interpret the first occurrence of )" as the end of the string? Yes, it will. But the raw string syntax allows you to place additional characters between the opening " and (.This implies that the same additional characters must appear between the final ) and ". So a raw string beginning with R"+* ( must terminate with )+*".Thus, the statement
+//*     - cout << R"+*("(Who wouldn't?)", she whispered.)+*" << endl;
+//* would display the following :
+//*     - "(Who wouldn't?)", she whispered.
+
+//* In short, the default delimiters of "( and )" have been replaced with "+*( and )+*".You can use any of the members of the basic character set as part of the delimiter other than the space, the left parenthesis, the right parenthesis, the backslash, and control characters such as a tab or a newline.
+
+//* The R prefix can be combined with the other string prefixes to produce raw strings of wchar_t and so on.It can be either the first or the last part of a compound prefix : Ru, UR, and so on.
+
+//! Structs in C++
+
+//* in C++ no need to say typedef before the struct as any struct is a typedef by defult
+//* string class object instead of a character array for the struct member
+
+//! Summery:
+
 //* Arrays, structures, and pointers are three C++ compound types.An array can hold several values, all of the same type, in a single data object. By using an index, or subscript, you can access the individual elements in an array.
 
 //* A structure can hold several values of different types in a single data object, and you can use the membership operator (.) to access individual members.The first step in using structures is to create a structure template that defines what members the structure holds. The name, or tag, for this template then becomes a new type identifier.You can then declare structure variables of that type.
