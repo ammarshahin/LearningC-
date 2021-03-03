0a9d672b94bdc49f164811fc6a88261ebdfc2b90
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-03-03 11:29:01 +0200

    finishing Ch8

--- src/CppPrimerPlus/chapter8.cpp
+++ src/CppPrimerPlus/chapter8.cpp
@@ -123,8 +123,98 @@ void Swap(job &, job &);
 //* Swap()      >> implicit instantiation
 //* Swap<int>() >> explicit instantiation
 
-//! overload resolution : The way compiler deals with function tempelates and function overloading
+//! Instantiations and Specializations
+
+//*  including a function template in your code does not in itself generate a function definition. It’s merely a plan for generating a function definition. When the compiler uses the template to generate a function definition for a particular type, the result is termed an instantiation of the template.
+
+//* implicit instantiation : because the compiler deduces the necessity for making the definition by noting that the program uses a Swap() function with int parameters.
+//* explicit instantiation.That means you can instruct the compiler to create a particular instantiation
+template void Swap<int>(int, int);   // explicit instantiation
+
+template <>
+void Swap<int>(int &, int &);   // explicit specialization
+
+template <>
+void Swap(int &, int &);   // explicit specialization
+
+//* The difference is that these last two declarations mean “Don’t use the Swap() template to generate a function definition. Instead, use a separate, specialized function definition explicitly defined for the int type.”These prototypes have to be coupled with their own function definitions.The explicit specialization declaration has <> after the keyword template, whereas the explicit instantiation omits the <>.
+
+//* Implicit instantiations, explicit instantiations, and explicit specializations collectively are termed specializations.What they all have in common is that they represent a function definition that uses specific types rather than one that is a generic description.
+
+//* Ex:
+
+template <class T>
+void Swap(T &, T &);   // template prototype
+
+template <>
+void Swap<job>(job &, job &);   // explicit specialization for job
+
+int main(void)
+{
+    template void Swap<char>(char &, char &);   // explicit instantiation for char
+
+    short a, b;
+    Swap(a, b);   // implicit template instantiation for short
+
+    job n, m;
+    Swap(n, m);   // use explicit specialization for job
+
+    char g, h;
+    Swap(g, h);   // use explicit template instantiation for char
+}
+
+//! overload resolution : The way compiler deals with function tempelates and function overloading.
 
 //* Phase 1—Assemble a list of candidate functions.These are functions and templatefunctions that have the same names as the called functions.
+
 //* Phase 2—From the candidate functions, assemble a list of viable functions.These are functions with the correct number of arguments and for which there is an implicit conversion sequence, which includes the case of an exact match for each type of actual argument to the type of the corresponding formal argument. For example, a function call with a type float argument could have that value converted to a double to match a type double formal parameter, and a template could generate an instantiation for float.
+
 //* Phase 3—Determine whether there is a best viable function. If so, you use that function.Otherwise, the function call is an error.
+
+//FIXME: The rest of the Chapter is pretty advanced stuff That can be read later.
+
+//! What’s That Type?
+//! The decltype Keyword (C++11)
+
+//* The C++11 solution is a new keyword: decltype. It can be used in this way:
+int x;
+decltype(x) y;   // make y the same type as x
+
+//* The argument to decltype can be an expression, so in the ft() example, we could use this code:
+decltype(x + y) xpy;   // make xpy the same type as x + y
+xpy = x + y;
+
+//* Alternatively, we could combine these two statements into an initialization:
+decltype(x + y) xpy = x + y;
+
+double xx = 4.4;
+decltype((xx)) r2 = xx;   // r2 is double &
+decltype(xx) w = xx;      // w is double (Stage 1 match)
+
+//! trailing return type :
+auto h(int x, float y) -> double;
+
+//* This moves the return type to after the parameter declarations.
+//! auto : is a placeholder for the type provided by the trailing return type.
+
+//* Combining this syntax with decltype leads to the following solution for specifying the return type for gt():
+
+template <class T1, class T2>
+auto gt(T1 x, T2 y) -> decltype(x + y)
+{
+    return x + y;
+}
+
+//* Now decltype comes after the parameter declarations, so x and y are in scope and can be used.
+
+//! Summary
+
+//* C++ has expanded C function capabilities. By using an inline keyword with a function definition and by placing that definition ahead of the first call to that function, you suggest to the C++ compiler that it make the function inline.That is, instead of having the program jump to a separate section of code to execute the function, the compiler replaces the function call with the corresponding code inline.An inline facility should be used only when the function is short.
+
+//* A reference variable is a kind of disguised pointer that lets you create an alias (that is, a second name) for a variable. Reference variables are primarily used as arguments to functions that process structures and class objects. Normally, an identifier declared as a reference to a particular type can refer only to data of that type. However, when one class is derived from another, such as ofstream from ostream, a reference to the base type may also refer to the derived type.
+
+//* C++ prototypes enable you to define default values for arguments. If a function call omits the corresponding argument, the program uses the default value. If the function includes an argument value, the program uses that value instead of the default. Default arguments can be provided only from right to left in the argument list.Thus, if you provide a default value for a particular argument, you must also provide default values for all arguments to the right of that argument.
+
+//* A function’s signature is its argument list.You can define two functions having the same name, provided that they have different signatures.This is called function polymorphism, or function overloading.Typically, you overload functions to provide essentially the same service to different data types.
+
+//* Function templates automate the process of overloading functions.You define a function by using a generic type and a particular algorithm, and the compiler generates appropriate function definitions for the particular argument types you use in a program.
