b631120cd43b7543116605e7e20c7a09c02c7374
ammarshahin <ammar0ammar612@gmail.com>
2022-12-31 19:08:49 +0200

    adding the algoritms lesson

---
+++ src/Mastering4CriticalSkillsUsingC++17/STLs/algorithms.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdint-gcc.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print(const vector<int> &v)
+{
+    cout << "v :";
+    for (auto &val : v)
+        cout << " " << val;
+
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> v{1, 2, 3, 4, 5, 8, 4, 2, 0};
+    print(v);
+
+    //! Sort
+    sort<vector<int>::iterator>(v.begin(), v.end());
+    print(v);
+
+    return 0;
+}
