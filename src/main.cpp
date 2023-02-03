eb4f422cd9c0cf8d757fed8cde0ce75760814d8f
ammarshahin <ammar0ammar612@gmail.com>
2023-02-03 21:09:45 +0200

    adding CI

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-#include <bits/stdc++.h>
-#include <mutex>
-#include <thread>
-#include <chrono>
 #include "lambda.hpp"
 #include "options.hpp"
 #include "SmartPointers.hpp"
-
-using namespace std;
 
 int main()
 {
