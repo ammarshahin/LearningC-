2a1d4d79be70f1e2aa2fb29a77d83db90d47d048
ashahin <ammar.shahin@swift-act.com>
2023-04-11 13:41:21 +0200

    removing warnnings

--- src/EffectiveC++/ch1.cpp
+++ src/EffectiveC++/ch1.cpp
@@ -5,11 +5,12 @@ const std::string &GetString();   // The returned string cannot be modified
 
 const std::string &GetString()   // The returned string cannot be modified
 {
-    static const std::string str = "ammarShahin";
+    static const std::string str = "Ammar Shahin";
     return str;
 }
 
 void ch1()
 {
     const std::string &name = GetString();
+    std::cout << "name: " << name << std::endl;
 }
